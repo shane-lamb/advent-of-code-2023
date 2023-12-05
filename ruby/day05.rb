944bb75f90527a665551708d2086166f8ef53195
Shane Lamb <shanelamb1@gmail.com>
2023-12-06 00:22:22 +0200

    day 5 part 1

--- ruby/day05.rb
+++ ruby/day05.rb
@@ -1,6 +1,44 @@
 def run_part_1(file_name)
   lines = get_lines(file_name)
-  lines.count
+  parts = lines.join("\n").split("\n\n").map { |section| section.split("\n") }
+  seeds = parts[0][0].split(" ")[1..].map(&:to_i)
+  almanac = Almanac.new(parts[1..])
+  locations = seeds.map { |id| almanac.get_mapped_id(id, 0, 6) }
+  locations.min
+end
+
+class Almanac
+  def initialize(raw_maps)
+    @levels = raw_maps.map do |lines|
+      ranges = lines[1..].map do |line|
+        dest_range_start, src_range_start, range_length = line.split(" ").map(&:to_i)
+        {
+          start: src_range_start,
+          end: src_range_start + range_length - 1,
+          offset: dest_range_start - src_range_start
+        }
+      end
+      ranges.sort_by { |range| range[:start] }
+    end
+  end
+
+  def get_mapped_id(original_id, start_index, end_index)
+    (start_index..end_index).reduce(original_id) do |mapped_id, current_index|
+      range = get_range(mapped_id, current_index)
+      next mapped_id if range == nil
+      mapped_id + range[:offset]
+    end
+  end
+
+  def get_range(id, index)
+    level = @levels[index]
+    level.each do |range|
+      next if range[:start] > id
+      next if id > range[:end]
+      return range
+    end
+    nil
+  end
 end
 
 def run_part_2(file_name)
