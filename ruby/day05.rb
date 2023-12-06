6c5249e2ecb83d60f83da52f5dd4a5a5e13ecb2d
Shane Lamb <shanelamb1@gmail.com>
2023-12-06 16:31:49 +0900

    day 5 part 2

--- ruby/day05.rb
+++ ruby/day05.rb
@@ -3,7 +3,7 @@ def run_part_1(file_name)
   parts = lines.join("\n").split("\n\n").map { |section| section.split("\n") }
   seeds = parts[0][0].split(" ")[1..].map(&:to_i)
   almanac = Almanac.new(parts[1..])
-  locations = seeds.map { |id| almanac.get_mapped_id(id, 0, 6) }
+  locations = seeds.map { |id| almanac.get_location_id(id)[:id] }
   locations.min
 end
 
@@ -22,28 +22,68 @@ class Almanac
     end
   end
 
-  def get_mapped_id(original_id, start_index, end_index)
-    (start_index..end_index).reduce(original_id) do |mapped_id, current_index|
-      range = get_range(mapped_id, current_index)
-      next mapped_id if range == nil
-      mapped_id + range[:offset]
+  def get_lowest_location_id(start_seed_id, end_seed_id)
+    current_seed_id = start_seed_id
+    lowest_location_id = nil
+    while current_seed_id <= end_seed_id do
+      location = get_location_id(current_seed_id)
+      min_to_next_range = location[:min_to_next_range]
+      id = location[:id]
+      lowest_location_id = id if lowest_location_id == nil
+      lowest_location_id = [lowest_location_id, id].min
+      break if min_to_next_range == nil
+      current_seed_id = current_seed_id + min_to_next_range
+    end
+    lowest_location_id
+  end
+
+  def get_location_id(seed_id)
+    end_index = @levels.count - 1
+    (0..end_index).reduce({ id: seed_id, min_to_next_range: nil }) do |agg, current_index|
+      old_id = agg[:id]
+      info = get_id_offset(old_id, current_index)
+      id = old_id + info[:id_offset]
+      min_to_next_range = if agg[:min_to_next_range] == nil
+                            info[:to_next_range]
+                          elsif info[:to_next_range] == nil
+                            agg[:min_to_next_range]
+                          else
+                            [agg[:min_to_next_range], info[:to_next_range]].min
+                          end
+      { id:, min_to_next_range: }
     end
   end
 
-  def get_range(id, index)
-    level = @levels[index]
-    level.each do |range|
-      next if range[:start] > id
+  def get_id_offset(id, level_index)
+    ranges = @levels[level_index]
+    ranges.each do |range|
       next if id > range[:end]
-      return range
+      if range[:start] > id
+        return { id_offset: 0, to_next_range: range[:start] - id }
+      else
+        return { id_offset: range[:offset], to_next_range: (range[:end] + 1) - id }
+      end
     end
-    nil
+    { id_offset: 0, to_next_range: nil }
   end
 end
 
 def run_part_2(file_name)
   lines = get_lines(file_name)
-  lines.count
+  parts = lines.join("\n").split("\n\n").map { |section| section.split("\n") }
+  seeds = parse_seeds(parts[0][0])
+  almanac = Almanac.new(parts[1..])
+  lowest_location_ids = seeds.map { |seed| almanac.get_lowest_location_id(seed[:start], seed[:end]) }
+  lowest_location_ids.min
+end
+
+def parse_seeds(seeds_text)
+  values = seeds_text.split(" ")[1..].map(&:to_i)
+  seeds = []
+  values.each_slice(2) do |pair|
+    seeds.push({ start: pair[0], end: pair[0] + pair[1] - 1 })
+  end
+  seeds
 end
 
 def get_lines(file_name)
@@ -54,7 +94,7 @@ end
 
 day_num = "05"
 
-part_1_test_result = run_part_1("day#{day_num}_test_part_1.txt")
+part_1_test_result = run_part_1("day#{day_num}_test.txt")
 part_1_expected_test_result = 35
 if part_1_test_result != part_1_expected_test_result
   puts "test failed! expected #{part_1_expected_test_result}, got #{part_1_test_result}"
@@ -63,11 +103,11 @@ end
 part_1_result = run_part_1("day#{day_num}_input.txt")
 puts "part 1 result: #{part_1_result}"
 
-# part_2_test_result = run_part_2("day#{day_num}_test_part_2.txt")
-# part_2_expected_test_result = 5678
-# if part_2_test_result != part_2_expected_test_result
-#   puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
-# end
-#
-# part_2_result = run_part_2("day#{day_num}_input.txt")
-# puts "part 2 result: #{part_2_result}"
+part_2_test_result = run_part_2("day#{day_num}_test.txt")
+part_2_expected_test_result = 46
+if part_2_test_result != part_2_expected_test_result
+  puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
+end
+
+part_2_result = run_part_2("day#{day_num}_input.txt")
+puts "part 2 result: #{part_2_result}"
