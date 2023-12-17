a9731ae86d7ac1ec859d0d9c267ed9ad99cd5d52
Shane Lamb <shanelamb1@gmail.com>
2023-12-17 18:58:08 +1000

    day 12 part 2

--- ruby/day12.rb
+++ ruby/day12.rb
@@ -1,48 +1,47 @@
 class DamageRow
-  def initialize(line)
+  def initialize(line, repeat = 1)
     symbols, numbers_text = line.split(" ")
-    @contiguous_damaged = numbers_text.split(",").map(&:to_i)
-    symbol_map = {
-      "#" => "0",
-      "." => "1",
-    }
-    unknown_count = 0
-    @sequence = symbols.split("").map do |char|
-      if char == "?"
-        fetch_index = unknown_count
-        unknown_count += 1
-        fetch_index
-      else
-        symbol_map[char]
-      end
-    end
-    @unknown_count = unknown_count
+    @contiguous_damaged = numbers_text.split(",").map(&:to_i) * repeat
+    symbols_repeated = ((symbols + "?") * repeat)[0...-1]
+    @sequence = symbols_repeated.split("")
   end
 
-  def generate_possibilities(length)
-    (0...2.pow(length)).each do |i|
-      yield i.to_s(2).rjust(length, "0").chars
+  def count_recursive(remaining_sequence, remaining_damaged, lookup)
+    cached = lookup[[remaining_sequence.count, remaining_damaged.count]]
+    return cached if cached
+    damage_count = remaining_damaged.first
+    if damage_count.nil?
+      return 0 if remaining_sequence.any? { |symbol| symbol == "#" }
+      return 1
     end
+    next_index = 0
+    possibility_count = 0
+    while next_index + damage_count <= remaining_sequence.count
+      current_index = next_index
+      next_index += 1
+      leading = remaining_sequence[0...current_index]
+      break if leading.any? { |symbol| symbol == "#" }
+      chunk = remaining_sequence[current_index...(current_index + damage_count)]
+      next if chunk.any? { |symbol| symbol == "." }
+      if current_index + damage_count < remaining_sequence.count
+        trailing = remaining_sequence[current_index + damage_count]
+        next if trailing == "#"
+        possibility_count += count_recursive(remaining_sequence[(current_index + damage_count + 1)..-1], remaining_damaged[1..-1], lookup)
+      else
+        possibility_count += count_recursive(remaining_sequence[(current_index + damage_count)..-1], remaining_damaged[1..-1], lookup)
+      end
+    end
+    lookup[[remaining_sequence.count, remaining_damaged.count]] = possibility_count
+    possibility_count
   end
 
   def count_possible_configurations
-    count = 0
-    generate_possibilities(@unknown_count) do |possibility|
-      to_check = []
-      broken_count = 0
-      @sequence.each do |symbol|
-        resolved = symbol.is_a?(Integer) ? possibility[symbol] : symbol
-        if resolved == "0"
-          broken_count += 1
-        elsif broken_count > 0
-          to_check << broken_count
-          broken_count = 0
-        end
-      end
-      to_check << broken_count if broken_count > 0
-      count += 1 if @contiguous_damaged == to_check
-    end
-    count
+    lookup = {}
+    count_recursive(@sequence, @contiguous_damaged, lookup)
+  end
+
+  def to_s
+    @sequence.join("") + " " + @contiguous_damaged.join(",")
   end
 end
 
@@ -53,6 +52,9 @@ def run_part_1(file_name)
 end
 
 def run_part_2(file_name)
+  lines = get_lines(file_name)
+  damage_rows = lines.map { |line| DamageRow.new(line, 5) }
+  damage_rows.sum(&:count_possible_configurations)
 end
 
 def get_grouped_lines(file_name, group_separator = "\n\n")
@@ -75,12 +77,12 @@ end
 
 part_1_result = run_part_1("day#{day_num}_input.txt")
 puts "part 1 result: #{part_1_result}"
-#
-# part_2_test_result = run_part_2("day#{day_num}_test.txt")
-# part_2_expected_test_result = 5678
-# if part_2_test_result != part_2_expected_test_result
-#   puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
-# end
-#
-# part_2_result = run_part_2("day#{day_num}_input.txt")
-# puts "part 2 result: #{part_2_result}"
+
+part_2_test_result = run_part_2("day#{day_num}_test.txt")
+part_2_expected_test_result = 525152
+if part_2_test_result != part_2_expected_test_result
+  puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
+end
+
+part_2_result = run_part_2("day#{day_num}_input.txt")
+puts "part 2 result: #{part_2_result}"
