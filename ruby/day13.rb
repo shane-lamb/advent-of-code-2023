9076b30f2ec4e74d7f707e4a1cd1323fe7a48f93
Shane Lamb <shanelamb1@gmail.com>
2023-12-17 20:39:17 +1000

    day 13 part 2

--- ruby/day13.rb
+++ ruby/day13.rb
@@ -7,29 +7,37 @@ def run_part_1(file_name)
   scores.sum
 end
 
-def get_score(grid)
+def get_score(grid, imperfections = 0)
   score = 0
-  horizontal_reflection_index = get_reflection_index(grid)
+  horizontal_reflection_index = get_reflection_index(grid, imperfections)
   score += horizontal_reflection_index * 100 if horizontal_reflection_index
-  vertical_reflection_index = get_reflection_index(grid.transpose)
+  vertical_reflection_index = get_reflection_index(grid.transpose, imperfections)
   score += vertical_reflection_index if vertical_reflection_index
   score
 end
 
-def get_reflection_index(grid)
+def get_reflection_index(grid, imperfections)
   (1...grid.length).each do |index|
     left = (index - 1).downto(0)
     right = (index...grid.length)
-    mirrored = left.zip(right).all? do |left_index, right_index|
-      next true if left_index.nil? || right_index.nil?
-      grid[left_index] == grid[right_index]
+    mirror_imperfections = left.zip(right).sum do |left_index, right_index|
+      next 0 if left_index.nil? || right_index.nil?
+      left_row = grid[left_index]
+      right_row = grid[right_index]
+      left_row.zip(right_row).count { |left_char, right_char| left_char != right_char }
     end
-    return index if mirrored
+    return index if mirror_imperfections == imperfections
   end
   nil
 end
 
 def run_part_2(file_name)
+  grouped_lines = get_grouped_lines(file_name)
+  scores = grouped_lines.map do |lines|
+    grid = lines.map { |line| line.split("") }
+    get_score(grid, 1)
+  end
+  scores.sum
 end
 
 def get_grouped_lines(file_name, group_separator = "\n\n")
@@ -52,12 +60,12 @@ end
 
 part_1_result = run_part_1("day#{day_num}_input.txt")
 puts "part 1 result: #{part_1_result}"
-#
-# part_2_test_result = run_part_2("day#{day_num}_test.txt")
-# part_2_expected_test_result = 5678
-# if part_2_test_result != part_2_expected_test_result
-#   puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
-# end
-#
-# part_2_result = run_part_2("day#{day_num}_input.txt")
-# puts "part 2 result: #{part_2_result}"
+
+part_2_test_result = run_part_2("day#{day_num}_test.txt")
+part_2_expected_test_result = 400
+if part_2_test_result != part_2_expected_test_result
+  puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
+end
+
+part_2_result = run_part_2("day#{day_num}_input.txt")
+puts "part 2 result: #{part_2_result}"
