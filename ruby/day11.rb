ae79ee8491209774332db6843007a523dea2688d
Shane Lamb <shanelamb1@gmail.com>
2023-12-17 10:00:20 +1000

    day 11 part 2

--- ruby/day11.rb
+++ ruby/day11.rb
@@ -34,10 +34,13 @@ end
 
 class Tile
   attr_reader :value, :pos
+  attr_accessor :width_expanded, :height_expanded
 
   def initialize(value, pos)
     @value = value
     @pos = pos
+    @width_expanded = false
+    @height_expanded = false
   end
 end
 
@@ -72,12 +75,38 @@ class Map
     tiles
   end
 
+  def get_distance(tile_1, tile_2, expansion_cost)
+    start_x = [tile_1.pos.x, tile_2.pos.x].min + 1
+    end_x = [tile_1.pos.x, tile_2.pos.x].max
+    x_cost = (start_x..end_x).sum do |x|
+      tile = get(Pos.new(x, tile_1.pos.y))
+      tile.width_expanded ? expansion_cost : 1
+    end
+
+    start_y = [tile_1.pos.y, tile_2.pos.y].min + 1
+    end_y = [tile_1.pos.y, tile_2.pos.y].max
+    y_cost = (start_y..end_y).sum do |y|
+      tile = get(Pos.new(tile_1.pos.x, y))
+      tile.height_expanded ? expansion_cost : 1
+    end
+
+    x_cost + y_cost
+  end
+
   def add_tile(tile)
     @tiles[tile.pos.y][tile.pos.x] = tile
   end
 
   def get(pos)
     @tiles[pos.y][pos.x]
+  end
+
+  def get_rows
+    @tiles
+  end
+
+  def get_cols
+    @tiles.transpose
   end
 
   def debug_output
@@ -114,9 +143,29 @@ def expand_char_grid(char_grid)
   expand_left.transpose
 end
 
-def run_part_2(file_name)
+def run_part_2(file_name, expansion_cost)
   char_grid = get_lines(file_name).map { |line| line.chars }
   map = Map.from_char_grid(char_grid)
+
+  # set expansion values
+  map.get_rows.each do |row|
+    all_empty = row.all? { |tile| tile.value == "." }
+    row.each { |tile| tile.height_expanded = all_empty }
+  end
+  map.get_cols.each do |col|
+    all_empty = col.all? { |tile| tile.value == "." }
+    col.each { |tile| tile.width_expanded = all_empty }
+  end
+
+  # calculate distances
+  tiles = map.find_all("#")
+  distances = tiles.map.with_index do |tile, index|
+    other_tiles = tiles[index + 1..-1]
+    other_tiles.sum do |other_tile|
+      map.get_distance(tile, other_tile, expansion_cost)
+    end
+  end
+  distances.sum
 end
 
 def get_grouped_lines(file_name, group_separator = "\n\n")
@@ -140,11 +189,11 @@ end
 part_1_result = run_part_1("day#{day_num}_input.txt")
 puts "part 1 result: #{part_1_result}"
 
-# part_2_test_result = run_part_2("day#{day_num}_test.txt")
-# part_2_expected_test_result = 8410
-# if part_2_test_result != part_2_expected_test_result
-#   puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
-# end
+part_2_test_result = run_part_2("day#{day_num}_test.txt", 100)
+part_2_expected_test_result = 8410
+if part_2_test_result != part_2_expected_test_result
+  puts "test failed! expected #{part_2_expected_test_result}, got #{part_2_test_result}"
+end
 
-# part_2_result = run_part_2("day#{day_num}_input.txt")
-# puts "part 2 result: #{part_2_result}"
+part_2_result = run_part_2("day#{day_num}_input.txt", 1000000)
+puts "part 2 result: #{part_2_result}"
